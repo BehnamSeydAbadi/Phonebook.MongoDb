7efdfb9b736dbe1b181daf6c29e1efba9191c623
Behnam S.A <behnamsa96@outlook.com>
2022-12-29 18:52:08 +0330

    Implement "InsertAsync_ShouldWorkSuccessfully"
    - State of "InsertAsync_ShouldWorkSuccessfully" is red
    - Install "AutoBogus" on "WebAPI.Test"
    - Install "FluentAssertions" on "WebAPI.Test"
    - Install "Microsoft.AspNetCore.Mvc.NewtonsoftJson" on "WebAPI.Test"
    - Create "ContactDto"
    - Create "OutputViewModel"

--- Phonebook.MongoDb/WebAPI.Test/Usings.cs
+++ Phonebook.MongoDb/WebAPI.Test/Usings.cs
@@ -1,2 +1,7 @@
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using WebAPI.Test.Common;
+global using AutoBogus;
+global using FluentAssertions;
+global using Newtonsoft.Json;
+global using System.Net.Http.Json;
+global using WebAPI.Common.ViewModels;
