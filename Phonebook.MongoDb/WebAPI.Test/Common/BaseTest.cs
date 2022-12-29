17d4945bdc19c9332bfd4d63f904fe79b29cdc52
Behnam S.A <behnamsa96@outlook.com>
2022-12-29 18:33:13 +0330

    Add "WebApplicationFactory" object to "BaseTest"

--- Phonebook.MongoDb/WebAPI.Test/Common/BaseTest.cs
+++ Phonebook.MongoDb/WebAPI.Test/Common/BaseTest.cs
@@ -1,6 +1,15 @@
-﻿namespace WebAPI.Test.Common
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebAPI.Test.Common
 {
     public abstract class BaseTest
     {
+        protected readonly HttpClient _httpClient;
+
+        public BaseTest()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            _httpClient = factory.CreateDefaultClient();
+        }
     }
 }
