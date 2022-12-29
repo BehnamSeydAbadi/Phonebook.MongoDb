5b3385652e36e58143ada374f2b399210a97929e
Behnam S.A <behnamsa96@outlook.com>
2022-12-29 17:56:26 +0330

    Create "Contact" entity
    - Create common classes like "Entity" and "IValueObject"
    - Create "FullName" value object.

---
+++ Phonebook.MongoDb/DataAccess/Contact/Entity/Contact.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Common.Entity;
+using DataAccess.Contact.ValueObjects;
+
+namespace DataAccess.Contact.Entity
+{
+    public sealed class Contact : BaseEntity
+    {
+        public FullName FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; init; }
+        public string Email { get; init; }
+    }
+}
