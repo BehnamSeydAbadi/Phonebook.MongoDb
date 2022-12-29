54c8d5a58212478d8daca9d0d271e5bf43f4c530
Behnam S.A <behnamsa96@outlook.com>
2022-12-29 18:11:36 +0330

    Create "CreateTests"
    - Create "BaseTest" for test infrastructure

---
+++ Phonebook.MongoDb/WebAPI.Test/Contact/CreateTests.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Test.Contact
+{
+    [TestClass]
+    public class CreateTests : BaseTest
+    {
+        [TestMethod]
+        public async Task InsertAsync_ShouldWorkSuccessfully()
+        {
+            
+        }
+    }
+}
