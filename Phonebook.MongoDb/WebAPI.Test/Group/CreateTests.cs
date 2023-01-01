bfb72c3f73965a8bb25cfe8b4f2c247233635294
Behnam S.A <behnamsa96@outlook.com>
2023-01-02 00:23:43 +0330

    Fail "InsertAsync_ShouldWorkSuccessfully" for group
    - Create "CreateTests"

---
+++ Phonebook.MongoDb/WebAPI.Test/Group/CreateTests.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+
+namespace WebAPI.Test.Group
+{
+    [TestClass]
+    public class CreateTests : BaseTest
+    {
+        public CreateTests() : base("api/groups") { }
+
+        [TestMethod]
+        public async Task InsertAsync_ShouldWorkSuccessfully()
+        {
+            var title = string.Empty;
+
+            var output = await PostAsync(title);
+
+            output.Data.ToString().Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
